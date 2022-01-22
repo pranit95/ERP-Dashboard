1bc9371747c8ddb7d08013c746ceceb6d802a502
pranit95 <pranit@sugarcosmetics.com>
2022-01-22 16:50:49 +0530

    changes in ui

--- frontend/src/Components/MiddleBox.jsx
+++ frontend/src/Components/MiddleBox.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import styles from './Styles/middleBox.module.css'
-import dataSet3 from "../Utils/summary.json"
+import dataSet3 from "../Utils/customers.json"
 
 const MiddleBox = () => {
   return (
@@ -8,14 +8,8 @@ const MiddleBox = () => {
       <div className={styles.mainContainer}>
         {dataSet3?.map((e) => (
           <li className={styles.boxContainer} key={e.id}>
-            <div className={styles.headRow}>
-              <div className={styles.headRowHeading}>{e.first_heading}</div>
               <div className={styles.headRowValue}>{e.first_value}</div>
-            </div>
-            <div className={styles.headRow}>
-              <div className={styles.headRowHeading}>{e.second_heading}</div>
-              <div className={styles.headRowValue}>{e.second_value}</div>
-            </div>
+              <div className={styles.headRowHeading}>{e.first_heading}</div>
           </li>
         ))}
       </div>
